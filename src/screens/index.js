07de7749f846d0cc4963153b96a2509b2d4b0db3
Mihaela Cretu <mihaela.cretzu@gmail.com>
2025-08-10 22:59:57 +0300

    Update App component to include HomeScreen and adjust initial screen state. Modify GestureHandlerRootView styles for improved layout. Add react-native-timeline-flatlist dependency in package.json and package-lock.json.

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
 export { default as SplitScreen } from './SplitScreen';
 export { default as ChooseYoursScreen } from './ChooseYoursScreen';
 export { default as TestNavigationScreen } from './TestNavigationScreen';
+export { default as HomeScreen } from './HomeScreen';
